32adb7d978dd2e0a7c63615f8cc84a8597674a24
Stroick <erik_o_mueller@yahoo.com>
2025-06-08 11:22:02 -0500

    feat: prep to deploy to Koyeb

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+// import { router as formRouter } from './routes/form.js';
+// import { router as indexRouter } from './routes/index.js';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/new', formRouter);
+// app.use('/', indexRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`)});
