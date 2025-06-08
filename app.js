1e336ff5e40542de02c166cb57c25636e9f5cc77
Stroick <erik_o_mueller@yahoo.com>
2025-06-08 12:39:55 -0500

    fix: update build script

--- app.js
+++ app.js
@@ -18,4 +18,4 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`)});
+app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`)}); 
