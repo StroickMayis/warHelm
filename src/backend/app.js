a358eb4b76103b64295feed9fda64444f8baaebb
Stroick <erik_o_mueller@yahoo.com>
2025-06-10 16:43:24 -0500

    fix: more prep to deploy to Koyeb

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,21 +1,20 @@
 import express from 'express';
 // import { router as formRouter } from './routes/form.js';
 // import { router as indexRouter } from './routes/index.js';
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 // app.use('/new', formRouter);
 // app.use('/', indexRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.json({ message: 'Hello World' });
 });
 
 app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`)}); 
